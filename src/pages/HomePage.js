8a51b11332d754979c62bbff9715f3303e1b0b0d
Suhrob Hasanov <suhrobhasanov1@gmail.com>
2023-02-28 22:17:03 -0800

    all

---
+++ src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function HomePage(){
+    return (
+        <>
+            <h2>HomePage</h2>
+            <p>Welcome to the Home Page</p>
+        </>
+    )
+}
+
+export default HomePage;
